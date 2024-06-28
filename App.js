aaad468e8a5f3ed24f6ad0b105374239b48689cb
pinkydevi12 <pinkybhatia48@gmail.com>
2024-06-28 16:14:07 +0530

    Bottom Tab Navigation is done

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from "react-native";
 
 import Navigation from "./src/navigation/stackNavigation/index";
+import { BottomNavigation } from "react-native-paper";
+import BottomTabNavigation from "./src/navigation/bottomNavigation";
 
 export default function App() {
   return (
     <>
-      <Navigation />
+      {/* <Navigation />*/}
+      <BottomTabNavigation />
     </>
   );
 }
---
+++ src/pages/DetailsScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const DetailsScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>DetailsScreen</Text>
+    </View>
+  );
+};
+
+export default DetailsScreen;
+
+const styles = StyleSheet.create({});
